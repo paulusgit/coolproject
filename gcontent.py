caf06701cccc2e5872ed2abb609708299d51f006
Paul <Paul@mailserver.com>
2015-11-27 20:25:28 +0100

    Alice version - Optimized code

--- gcontent.py
+++ gcontent.py
@@ -1,20 +1,25 @@
 def GCcontent (filename):
-	file = open(filename)
-	data = file.read()
-	datasplit = data.split("\n")
-	data2 = "".join(datasplit)
-	entries = []
-	values = []
-	for i,n in enumerate(data2):
-		if n == ">":
-			entries.append(i)
-	entries.append(len(data2))
-	w = 0
-	for i,n in enumerate(data2):
-		if n == ">":
-			w = w+1
-			GC = (data2.count("G",i,entries[w])+data2.count("C",i,entries[w]))*100/(entries[w]-i-14)
-			values.append(data2[i:i+14]) # El +14 reflecteix els espais que ocupa el codi
-			values.append(GC)
-	mGC = max(values[1::2])
-	print(values[values.index(mGC)-1],mGC)
+# My name is Alice and I am also part of the team.
+	def get_sequences(file):
+	list = [] 
+	header= file.readline()
+	while len(header) != 0:
+		seq = []
+		seq.append(header[1:-1])
+		r = ''
+		while True:
+			s=file.readline()
+			if len(s)==0 or s[0] == '>':
+				header = s
+				break
+			r=r+s[:-1]
+		seq.append(r)
+		list.append(seq)
+	return list
+	
+	def GC_content(seq):
+	cnt=0
+	for c in seq:
+	if c == 'C' or c == 'G':
+		cnt = cnt+1
+	return float(cnt) / len(seq)
